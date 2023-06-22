4eabd5df9dbc02aa78e64638061dd18919fb0ca9
Ensar Balık <ensarbalk@gmail.com>
2023-06-22 17:38:30 +0300

    User Data

--- Assets/Scripts/Sales Area/BuySalesArea.cs
+++ Assets/Scripts/Sales Area/BuySalesArea.cs
@@ -1,27 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Game.User;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Serialization;
 
 public class BuySalesArea : MonoBehaviour
 {
     public SalesAreaController saleArea;
     public TextMeshPro SellAreaText;
     public SpriteRenderer SellAreaImage;
-    public bool isSaled;
+    public bool isSold;
     public int cost;
+    public string id;
+
+    private void Start()
+    {
+        if (id == "SellArea1")
+        {
+            if (PlayerPrefs.GetFloat(id) == 0)
+            {
+                PlayerPrefs.SetFloat(id, 1f);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (PlayerPrefs.GetFloat(id) == 1f)
+            isSold = true;
+        else
+            isSold = false;
+
+        if (isSold)
+        {
+            SellAreaImage.color = Color.white;
+            SellAreaText.color = Color.white;
+        }
+    }
 
     private void Sale(int Cost)
     {
+        if (isSold) return;
         Cost = cost;
         if (PlayerPrefs.GetInt("myCoin") >= Cost)
         {
             SellAreaImage.color = Color.white;
             SellAreaText.color = Color.white;
             GameManager.instance.SpendMoney(cost);
-            isSaled = true;
+            isSold = true;
+            PlayerPrefs.SetFloat(id, 1f);
         }
     }
 
@@ -29,7 +59,7 @@ public class BuySalesArea : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (isSaled) return;
+            if (isSold) return;
             Sale(cost);
         }
     }
