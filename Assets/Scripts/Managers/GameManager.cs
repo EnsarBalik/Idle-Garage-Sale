35384a5c1f9c4c2507e40e172a546bd11b29d1e1
Ensar Balık <ensarbalk@gmail.com>
2023-06-22 15:48:48 +0300

    money transactions

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int coin;
+    [SerializeField] private TextMeshProUGUI coinText;
+    
+    private void LateUpdate()
     {
-        
+        coin = PlayerPrefs.GetInt("myCoin", coin);
+        coinText.text = "" + coin;
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void CollectCoin(int coinCollect)
     {
+        coin += coinCollect;
+        StartCoroutine(_coinText());
+        PlayerPrefs.SetInt("myCoin", coin);
+    }
+    
+    private IEnumerator _coinText()
+    {
+        float totalTime = 2f;
+        float elapsedTime = 0;
+        int coinAmount = PlayerPrefs.GetInt("myCoin");
+        float step = (coin - coinAmount) / totalTime;
         
+        while (elapsedTime < totalTime)
+        {
+            elapsedTime += Time.deltaTime;
+            int amount = Mathf.Clamp(coinAmount + Mathf.RoundToInt(step * elapsedTime), 0, coin);
+            coinText.text = amount.ToString();
+            yield return null;
+        }
     }
 }
